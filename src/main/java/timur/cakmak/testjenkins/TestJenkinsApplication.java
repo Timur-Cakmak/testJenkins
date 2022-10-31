a77a40a5d649c56347eafaa8c4edf434b45025be
Timur-Cakmak <timur.ck@gmail.com>
2022-10-31 14:49:52 +0100

    add_test

--- src/main/java/timur/cakmak/testjenkins/TestJenkinsApplication.java
+++ src/main/java/timur/cakmak/testjenkins/TestJenkinsApplication.java
@@ -1,13 +1,20 @@
 package timur.cakmak.testjenkins;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import timur.cakmak.product.ProductService;
+
 @SpringBootApplication
 public class TestJenkinsApplication {
+	
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestJenkinsApplication.class, args);
 	}
+	
+	
 
 }
