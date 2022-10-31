b050a5bdf7c374526223df76544382598f91e611
Timur-Cakmak <timur.ck@gmail.com>
2022-10-31 14:52:36 +0100

    add_test

--- src/test/java/timur/cakmak/testjenkins/TestJenkinsApplicationTests.java
+++ src/test/java/timur/cakmak/testjenkins/TestJenkinsApplicationTests.java
@@ -1,30 +1,30 @@
-//package timur.cakmak.testjenkins;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.Assert.assertNotNull;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import timur.cakmak.product.Product;
-//import timur.cakmak.product.ProductRepository;
+package timur.cakmak.testjenkins;
 
-//@SpringBootTest
-//class TestJenkinsApplicationTests {
-//	
-//	@Autowired
-//	ProductRepository productRepository;
-//
-//	@Test
-//	public void testCreate() {
-//		Product product = new Product();
-//		product.setId(1L);
-//		product.setName("Headphone");
-//		product.setDescription("good");
-//		product.setPrice(200.00);
-//		productRepository.save(product);
-////		assertThat(product).isEqualTo(productRepository.findById(1L).get());
-//		assertNotNull(productRepository.findById(1L).get());
-//	}
-//}
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import timur.cakmak.product.Product;
+import timur.cakmak.product.ProductRepository;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+	
+	@Autowired
+	ProductRepository productRepository;
+
+	@Test
+	public void testCreate() {
+		Product product = new Product();
+		product.setId(1L);
+		product.setName("Headphone");
+		product.setDescription("good");
+		product.setPrice(200.00);
+		productRepository.save(product);
+//		assertThat(product).isEqualTo(productRepository.findById(1L).get());
+		assertNotNull(productRepository.findById(1L).get());
+	}
+}
